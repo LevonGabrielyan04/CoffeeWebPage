e433b63863f6352050358b85373c53325f76e1c4
Levon Gabrielyan <levongabrielyan44@gmail.com>
2025-04-18 17:24:12 +0400

    .

--- resources/views/ribbon.blade.php
+++ resources/views/ribbon.blade.php
@@ -2,12 +2,14 @@
 
 </head>
 <body>
-    <div class="ribbon">
+    <div class="ribbon font-syne">
         <div class="ribbon-content">
             <div class = "ribbon-title">Cart</div>
             @foreach ($products as $item)
                 <div class = "ribbon-item">
-                    <span class = "ribbon-item-name font-syne">{{ $item->name }} - {{ $item->pivot->quantity }}</span>
+                    <img class = "ribbon-item-image" src="{{ asset('storage/' . $item->image) }}" alt="Product Image" />
+                    <span class = "ribbon-item-name font-syne">{{ $item->name }} | {{ $item->pivot->quantity }} items</span>
+                <hr style = "width: 100%; background-color:white;">
                 </div>
             @endforeach
             <span>Total price: {{$total_price}}$</span>
